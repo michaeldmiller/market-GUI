a9a2f0b3d08b3a711a7218f8a40356116f8cdc61
michaeldmiller <michaeldmiller256@gmail.com>
2022-06-09 08:53:42 -0400

    v0.1.9 abstracts scrolling graph functions

--- core/src/com/michaeldmiller/marketUI/MainInterface.java
+++ core/src/com/michaeldmiller/marketUI/MainInterface.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -15,12 +14,10 @@ import com.michaeldmiller.economicagents.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 import static com.michaeldmiller.economicagents.MarketMain.*;
 
 public class MainInterface implements Screen {
-
     final MarketUI marketUI;
     Stage stage;
     Skin firstSkin;
@@ -33,15 +30,7 @@ public class MainInterface implements Screen {
     int scale;
     int frame;
     double secondFraction;
-    int priceX;
-    int priceY;
-    int priceWidth;
-    int priceHeight;
-    ArrayList<GraphPoint> priceDots;
-    ArrayList<Label> priceLabels;
-
-
-
+    ScrollingGraph priceGraph;
 
     public MainInterface (final MarketUI marketUI) {
         this.marketUI = marketUI;
@@ -49,12 +38,6 @@ public class MainInterface implements Screen {
         frame = 0;
         secondFraction = 0.0167;
         scale = 3;
-        priceX = (int) (0.05 * marketUI.worldWidth);
-        priceY = (int) (0.1 * marketUI.worldHeight);
-        priceWidth = (int) (0.65 * marketUI.worldWidth);
-        priceHeight = (int) (0.7 * marketUI.worldHeight);
-        priceDots = new ArrayList<GraphPoint>();
-        priceLabels = new ArrayList<Label>();
 
         // setup color lookup table
         colorLookup = new HashMap<String, Color>();
@@ -75,10 +58,12 @@ public class MainInterface implements Screen {
         makeAdjustmentFields();
 
         // add price graph
-        makePriceGraph();
+        priceGraph = new ScrollingGraph((int) (0.05 * marketUI.worldWidth), (int) (0.1 * marketUI.worldHeight),
+                (int) (0.65 * marketUI.worldWidth), (int) (0.7 * marketUI.worldHeight), scale,
+                new HashMap<String, Integer>(), colorLookup, firstSkin, frame, stage);
+
+        priceGraph.makeGraph();
     }
-
-
 
     @Override
     public void show() {
@@ -97,18 +82,41 @@ public class MainInterface implements Screen {
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            graphPrices();
-            removeGraphDots(priceX, priceDots);
-            removeGraphLabels(priceX, priceLabels);
+            updatePriceGraph();
             prices.setText(market.getPrices().toString());
 
         }
-        priceGraphLabels();
+        priceGraph.graphLabels();
         stage.act(delta);
         stage.draw();
     }
 
+    @Override
+    public void resize(int width, int height) {
 
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    // UI Instantiation
     public void addButtons(){
         Button menuButton = new TextButton("Menu", firstSkin);
         menuButton.setPosition(marketUI.worldWidth - marketUI.standardButtonWidth,
@@ -134,7 +142,7 @@ public class MainInterface implements Screen {
         printButton.addListener(new InputListener(){
             @Override
             public void touchUp (InputEvent event, float x, float y, int pointer, int button){
-                System.out.println(priceDots.size());
+                System.out.println(priceGraph.getDots().size());
             }
             @Override
             public boolean touchDown (InputEvent event, float x, float y, int pointer, int button){
@@ -142,33 +150,6 @@ public class MainInterface implements Screen {
             }
         });
         stage.addActor(printButton);
-
-    }
-
-    public void instantiateMarket(){
-        MarketInfo fish = new MarketInfo("Fish", 0.35, -0.5, 0.7,
-                9, 1, "Fisherman", 0.4);
-        MarketInfo lumber = new MarketInfo("Lumber", 0.2, -0.5, 0.8,
-                15, 1, "Lumberjack", 0.2);
-        MarketInfo grain = new MarketInfo("Grain", 0.45, -0.5, 0.4,
-                7, 1, "Farmer", 0.4);
-        MarketInfo metal = new MarketInfo("Metal", 0.10, -1.2, 1.5,
-                50, 1, "Blacksmith", 0.05);
-        ArrayList<MarketInfo> currentMarketProfile = new ArrayList<MarketInfo>();
-        currentMarketProfile.add(fish);
-        currentMarketProfile.add(lumber);
-        currentMarketProfile.add(grain);
-        currentMarketProfile.add(metal);
-
-        int numberOfAgents = 2000;
-        ArrayList<Agent> marketAgents = new ArrayList<Agent>();
-        marketAgents = makeAgents(currentMarketProfile, numberOfAgents);
-
-        market = makeMarket(currentMarketProfile, marketAgents);
-
-        prices = new Label ("Prices", firstSkin);
-        prices.setPosition(100, marketUI.worldHeight - 50);
-        stage.addActor(prices);
 
     }
 
@@ -204,7 +185,37 @@ public class MainInterface implements Screen {
         stage.addActor(errorLabel);
 
     }
+    // Market instantiation
+    public void instantiateMarket(){
+        // set up market profile
+        MarketInfo fish = new MarketInfo("Fish", 0.35, -0.5, 0.7,
+                9, 1, "Fisherman", 0.4);
+        MarketInfo lumber = new MarketInfo("Lumber", 0.2, -0.5, 0.8,
+                15, 1, "Lumberjack", 0.2);
+        MarketInfo grain = new MarketInfo("Grain", 0.45, -0.5, 0.4,
+                7, 1, "Farmer", 0.4);
+        MarketInfo metal = new MarketInfo("Metal", 0.10, -1.2, 1.5,
+                50, 1, "Blacksmith", 0.05);
+        ArrayList<MarketInfo> currentMarketProfile = new ArrayList<MarketInfo>();
+        currentMarketProfile.add(fish);
+        currentMarketProfile.add(lumber);
+        currentMarketProfile.add(grain);
+        currentMarketProfile.add(metal);
 
+        // set number of agents
+        int numberOfAgents = 2000;
+        // create agents
+        ArrayList<Agent> marketAgents = makeAgents(currentMarketProfile, numberOfAgents);
+        // create market
+        market = makeMarket(currentMarketProfile, marketAgents);
+
+        prices = new Label ("Prices", firstSkin);
+        prices.setPosition(100, marketUI.worldHeight - 50);
+        stage.addActor(prices);
+
+    }
+
+    // modifier function
     public void changePrice(){
         // given information in good and cost text fields, attempt to change the corresponding cost in the market
         boolean costOK = false;
@@ -230,153 +241,17 @@ public class MainInterface implements Screen {
         }
 
     }
-
-    public void removeGraphDots(int xThreshold, ArrayList<GraphPoint> dots){
-        // given the list of graph points
-        for (int i = 0; i < dots.size(); i++){
-            // if the x coordinate of the dot is at or has surpassed the threshold plus one (moving right to left)
-            // remove the dot from the stage and the list of dots
-            if (dots.get(i).getX() <= xThreshold + 3){
-                dots.get(i).remove();
-                dots.remove(i);
-            }
-        }
-    }
-
-    public void removeGraphLabels(int xThreshold, ArrayList<Label> labels){
-        // essentially identical to removeGraphDots but for Labels, may be redundant
-        for (int i = 0; i < labels.size(); i++){
-            if (labels.get(i).getX() <= xThreshold + 3){
-                labels.get(i).remove();
-                labels.remove(i);
-            }
-        }
-    }
-
-    public void graphPrices(){
-        // access and store all current prices, adjusted to scale with the size of the graph
+    // graph update functions
+    public void updatePriceGraph(){
+        // update function for the price graph, gets price data from market and then turns it into coordinates
+        priceGraph.setFrame(frame);
         HashMap<String, Integer> priceCoordinates = new HashMap<String, Integer>();
         for (Price p : market.getPrices()){
-            priceCoordinates.put(p.getGood(), (int) p.getCost() * scale);
+            priceCoordinates.put(p.getGood(), (int) p.getCost() * priceGraph.getScale());
         }
-        // for each price coordinate pair, lookup the appropriate color, make a dot on the graph, then set it to scroll
-        // off to the left of the screen
-        for (Map.Entry<String, Integer> priceCoord : priceCoordinates.entrySet()){
-            // find color
-            Color dotColor = colorLookup.get(priceCoord.getKey());
-            // make dot
-            GraphPoint dot = new GraphPoint(priceX + priceWidth, priceCoord.getValue() + priceY, 2, 2, dotColor);
-
-            // make actor leave screen
-            MoveToAction leaveScreen = new MoveToAction();
-            leaveScreen.setPosition(priceX - 10, priceY + priceCoord.getValue());
-            leaveScreen.setDuration(50);
-            dot.addAction(leaveScreen);
-            // add actor to list of price dots
-            priceDots.add(dot);
-
-            stage.addActor(dot);
-
-        }
-    }
-
-    public void makePriceGraph(){
-        // using GraphPoints as they are general rectangles and are suited to the task
-        // add x-axis
-        GraphPoint xAxis = new GraphPoint(priceX, priceY, priceWidth, 3, new Color (0, 0, 0, 1));
-        stage.addActor(xAxis);
-        // add y-axis
-        GraphPoint yAxis = new GraphPoint(priceX, priceY, 3, priceHeight, new Color (0, 0, 0, 1));
-        stage.addActor(yAxis);
-        // add x-ceiling
-        GraphPoint xCeiling = new GraphPoint(priceX, priceY + priceHeight,
-                priceWidth, 2, new Color (0, 0, 0, 1));
-        stage.addActor(xCeiling);
-        // add y-ceiling
-        GraphPoint yCeiling = new GraphPoint(priceX + priceWidth, priceY,
-                2, priceHeight, new Color (0, 0, 0, 1));
-        stage.addActor(yCeiling);
-
-    }
-
-    public void priceGraphLabels(){
-        if (frame % 200 == 0 || frame == 1){
-            Label timeLabel = new Label(String.valueOf(frame), firstSkin);
-            timeLabel.setPosition(priceX + priceWidth, priceY - 20);
-
-            // add action for the labels to move to the left, following the dots
-            // it is important that the duration for dot movement and label movement be the same
-            MoveToAction leaveScreen = new MoveToAction();
-            leaveScreen.setPosition(priceX - 10, priceY - 20);
-            leaveScreen.setDuration(50);
-            timeLabel.addAction(leaveScreen);
-            // add labels to price label list for later removal
-            priceLabels.add(timeLabel);
-
-            stage.addActor(timeLabel);
-        }
-
-        if (frame == 1){
-            // set range of prices to be covered in the graph
-            int priceMax = priceHeight/scale;
-            int labelNum = 1;
-
-            for (int i = priceX; i < (priceHeight + priceX); i++) {
-                // split price height into 10 sections
-                if (i % priceHeight / 10 == 0){
-                    // set label value to tenth of priceMax * labelNum, set position at labelNum * 1/10th of the way
-                    // up the graph
-                    Label quantityLabel = new Label(String.valueOf(priceMax / 10 * labelNum), firstSkin);
-                    quantityLabel.setPosition(priceX + priceWidth - 20, priceY +  ((int) (priceHeight / 10) * labelNum) - 10);
-                    // quantityLabel.setAlignment(Align.right);
-                    // ^doesn't work
-
-                    // add action for the labels to move to the left, following the dots. Unlike dots and tick number
-                    // labels, the price labels will not be deleted upon reaching their resting place on the y-axis
-                    // it is again important that the duration for dot movement and label movement be the same
-                    MoveToAction leaveScreen = new MoveToAction();
-                    leaveScreen.setPosition(priceX - 25, priceY + ((int) (priceHeight / 10) * labelNum) - 10);
-                    leaveScreen.setDuration(50);
-                    quantityLabel.addAction(leaveScreen);
-
-                    stage.addActor(quantityLabel);
-
-                    // add horizontal guides
-                    GraphPoint xGuide = new GraphPoint(priceX, priceY + ((int) (priceHeight / 10) * labelNum),
-                            priceWidth, 1, new Color (0, 0, 0, 1));
-                    stage.addActor(xGuide);
-                    labelNum += 1;
-
-
-
-                }
-            }
-        }
-    }
-
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-
+        priceGraph.setDataCoordinates(priceCoordinates);
+        priceGraph.graphData();
+        priceGraph.removeGraphDots(priceGraph.getX(), priceGraph.getDots());
+        priceGraph.removeGraphLabels(priceGraph.getX(), priceGraph.getLabels());
     }
 }
---
+++ core/src/com/michaeldmiller/marketUI/ScrollingGraph.java
@@ -0,0 +1,237 @@
+package com.michaeldmiller.marketUI;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScrollingGraph {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int scale;
+    private HashMap<String, Integer> dataCoordinates;
+    private HashMap<String, Color> colorLookup;
+    private Skin skin;
+    private int frame;
+    private Stage stage;
+    private ArrayList<GraphPoint> dots;
+    private ArrayList<Label> labels;
+
+    public ScrollingGraph(int x, int y, int width, int height, int scale, HashMap<String, Integer> dataCoordinates,
+                          HashMap<String, Color> colorLookup, Skin skin, int frame, Stage stage){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.scale = scale;
+        this.dataCoordinates = dataCoordinates;
+        this.colorLookup = colorLookup;
+        this.skin = skin;
+        this.frame = frame;
+        this.stage = stage;
+        this.dots = new ArrayList<GraphPoint>();
+        this.labels = new ArrayList<Label>();
+    }
+    // accessors
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public int getWidth(){
+        return width;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public int getScale(){
+        return scale;
+    }
+    public HashMap<String, Integer> getDataCoordinates(){
+        return dataCoordinates;
+    }
+    public HashMap<String, Color> getColorLookup(){
+        return colorLookup;
+    }
+    public Skin getSkin(){
+        return skin;
+    }
+    public int getFrame(){
+        return frame;
+    }
+    public Stage getStage(){
+        return stage;
+    }
+    public ArrayList<GraphPoint> getDots() {
+        return dots;
+    }
+    public ArrayList<Label> getLabels() {
+        return labels;
+    }
+    // Mutators
+    public void setX(int newX) {
+        this.x = newX;
+    }
+    public void setY(int newY) {
+        this.y = newY;
+    }
+    public void setWidth(int newWidth) {
+        this.width = newWidth;
+    }
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+    public void setScale(int newScale) {
+        this.scale = newScale;
+    }
+    public void setDataCoordinates(HashMap<String, Integer> newDataCoordinates) {
+        this.dataCoordinates = newDataCoordinates;
+    }
+    public void setColorLookup(HashMap<String, Color> newColorLookup) {
+        this.colorLookup = newColorLookup;
+    }
+    public void setSkin(Skin newSkin) {
+        this.skin = newSkin;
+    }
+    public void setFrame(int newFrame) {
+        this.frame = newFrame;
+    }
+    public void setStage(Stage newStage) {
+        this.stage = newStage;
+    }
+    public void setDots(ArrayList<GraphPoint> newDots) {
+        this.dots = newDots;
+    }
+    public void setLabels(ArrayList<Label> newLabels) {
+        this.labels = newLabels;
+    }
+
+    public void graphData(){
+        // access and store all current prices, adjusted to scale with the size of the graph
+
+        // for each price coordinate pair, lookup the appropriate color, make a dot on the graph, then set it to scroll
+        // off to the left of the screen
+        for (Map.Entry<String, Integer> priceCoord : dataCoordinates.entrySet()){
+            // find color
+            Color dotColor = colorLookup.get(priceCoord.getKey());
+            // make dot
+            GraphPoint dot = new GraphPoint(x + width, priceCoord.getValue() + y, 2, 2, dotColor);
+
+            // make actor leave screen
+            MoveToAction leaveScreen = new MoveToAction();
+            leaveScreen.setPosition(x - 10, y + priceCoord.getValue());
+            leaveScreen.setDuration(50);
+            dot.addAction(leaveScreen);
+            // add actor to list of price dots
+            dots.add(dot);
+
+            stage.addActor(dot);
+
+        }
+    }
+
+    public void makeGraph(){
+        // using GraphPoints as they are general rectangles and are suited to the task
+        // add x-axis
+        GraphPoint xAxis = new GraphPoint(x, y, width, 3, new Color (0, 0, 0, 1));
+        stage.addActor(xAxis);
+        // add y-axis
+        GraphPoint yAxis = new GraphPoint(x, y, 3, height, new Color (0, 0, 0, 1));
+        stage.addActor(yAxis);
+        // add x-ceiling
+        GraphPoint xCeiling = new GraphPoint(x, y + height,
+                width, 2, new Color (0, 0, 0, 1));
+        stage.addActor(xCeiling);
+        // add y-ceiling
+        GraphPoint yCeiling = new GraphPoint(x + width, y,
+                2, height, new Color (0, 0, 0, 1));
+        stage.addActor(yCeiling);
+
+    }
+
+    public void graphLabels(){
+        if (frame % 200 == 0 || frame == 1){
+            Label timeLabel = new Label(String.valueOf(frame), skin);
+            timeLabel.setPosition(x + width, y - 20);
+
+            // add action for the labels to move to the left, following the dots
+            // it is important that the duration for dot movement and label movement be the same
+            MoveToAction leaveScreen = new MoveToAction();
+            leaveScreen.setPosition(x - 10, y - 20);
+            leaveScreen.setDuration(50);
+            timeLabel.addAction(leaveScreen);
+            // add labels to price label list for later removal
+            labels.add(timeLabel);
+
+            stage.addActor(timeLabel);
+        }
+
+        if (frame == 1){
+            // set range of prices to be covered in the graph
+            int priceMax = height/scale;
+            int labelNum = 1;
+
+            for (int i = x; i < (height + x); i++) {
+                // split price height into 10 sections
+                if (i % height / 10 == 0){
+                    // set label value to tenth of priceMax * labelNum, set position at labelNum * 1/10th of the way
+                    // up the graph
+                    Label quantityLabel = new Label(String.valueOf(priceMax / 10 * labelNum), skin);
+                    quantityLabel.setPosition(x + width - 20, y +  ((int) (height / 10) * labelNum) - 10);
+                    // quantityLabel.setAlignment(Align.right);
+                    // ^doesn't work
+
+                    // add action for the labels to move to the left, following the dots. Unlike dots and tick number
+                    // labels, the price labels will not be deleted upon reaching their resting place on the y-axis
+                    // it is again important that the duration for dot movement and label movement be the same
+                    MoveToAction leaveScreen = new MoveToAction();
+                    leaveScreen.setPosition(x - 25, y + ((int) (height / 10) * labelNum) - 10);
+                    leaveScreen.setDuration(50);
+                    quantityLabel.addAction(leaveScreen);
+
+                    stage.addActor(quantityLabel);
+
+                    // add horizontal guides
+                    GraphPoint xGuide = new GraphPoint(x, y + ((int) (height / 10) * labelNum),
+                            width, 1, new Color (0, 0, 0, 1));
+                    stage.addActor(xGuide);
+                    labelNum += 1;
+
+
+
+                }
+            }
+        }
+    }
+
+    // remove functions
+    public void removeGraphDots(int xThreshold, ArrayList<GraphPoint> dots){
+        // given the list of graph points
+        for (int i = 0; i < dots.size(); i++){
+            // if the x coordinate of the dot is at or has surpassed the threshold plus one (moving right to left)
+            // remove the dot from the stage and the list of dots
+            if (dots.get(i).getX() <= xThreshold + 3){
+                dots.get(i).remove();
+                dots.remove(i);
+            }
+        }
+    }
+
+    public void removeGraphLabels(int xThreshold, ArrayList<Label> labels){
+        // essentially identical to removeGraphDots but for Labels, may be redundant
+        for (int i = 0; i < labels.size(); i++){
+            if (labels.get(i).getX() <= xThreshold + 3){
+                labels.get(i).remove();
+                labels.remove(i);
+            }
+        }
+    }
+}
